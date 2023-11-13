20e526240a4626a07049f1066f6fabc1d8e0e6d9
Dia <142447820+marksofdespair@users.noreply.github.com>
2023-11-13 11:01:14 -0600

    Completed GA5 (in theory)

--- script.js
+++ script.js
@@ -1,42 +1,40 @@
-// Write your JavaScript code here!
 window.addEventListener("load", function() {
+    const form = document.querySelector("form")
+    
+    // Below this comment call the appropriate helper functions
+    form.addEventListener("submit", event=>{
+        event.preventDefault(); // Prevents default form submission
+            let pilot = document.querySelector("input[name=pilotName]").value;
+            let copilot = document.querySelector("input[name=copilotName]").value;
+            let fuelLevel = document.querySelector("input[name=fuelLevel]").value;
+            let cargoLevel = document.querySelector("input[name=cargoMass]").value;
+            let list = document.getElementById("faultyItems");
+            
+        formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) // Moved formsub call up here or it wont work????
+
+    })
+
+    // Calls the asynchronous function myFetch to fetch planetary data.
+    // Stores the fetched data in the listedPlanets variable.
     let listedPlanets;
-    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
     let listedPlanetsResponse = myFetch();
 
-    listedPlanetsResponse.then(function(result) {
+    listedPlanetsResponse.then(function (result) {
         listedPlanets = result;
         console.log(listedPlanets);
-
-        // Below this comment call the appropriate helper functions
+    }).then(function () {
+        console.log(listedPlanets);
         // to pick a planet from the list of planets and add that information to your destination.
-        const selectedPlanet = pickPlanet(listedPlanets);
-
-        const name = selectedPlanet.name;
-        const diameter = selectedPlanet.diameter;
-        const star = selectedPlanet.star;
-        const distance = selectedPlanet.distance;
-        const moons = selectedPlanet.moons;
-        const imageUrl = selectedPlanet.image;
+        let planet = pickPlanet(listedPlanets);
+        let name = planet.name;
+        let diameter = planet.diameter;
+        let star = planet.star;
+        let distance = planet.distance;
+        let imageUrl = planet.image;
+        let moons = planet.moons;
 
         // Update the mission target div with the selected planet's information
         addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl);
-
-        // Gets values from the form
-        const pilotName = document.getElementById("pilotName").value;
-        const coPilotName = document.getElementById("copilotName").value;
-        const fuelLevel = document.getElementById("fuelLevel").value;
-        const cargoMass = document.getElementById("cargoMass").value;
-
-        // Call formSubmission with the selected planet's data and form values
-        formSubmission(
-            document,
-            listedPlanets,
-            pilotName,
-            coPilotName,
-            fuelLevel,
-            cargoMass,
-            document.getElementById("launchStatus")
-        );        
-    });
-});
+    })
+    
+ });
--- scriptHelper.js
+++ scriptHelper.js
@@ -33,16 +33,12 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
  }
  
  function validateInput(testInput) {
-    if (testInput === "") {
-        return "Empty";
-    } else if (isNaN(testInput)) {
-        return isNaN(parseFloat(testInput)) ? "Not a Number" : "Is a Number";
-    } else {
-        return "Is a Number";
-    }
- }
+    // Previously used if/else statements here but it wasn't really working with the rest of the code
+        return testInput === "" ? "Empty" : isNaN(testInput) ? "Not a Number" : "Is a Number";
+     };
  
-function formSubmission(document, faultyItems, pilot, copilot, fuelLevel, cargoLevel) {
+function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
+    /* Why tf do if/else keep breaking everything :)
     // Validate input
     const pilotValidation = validateInput(pilot);
     const copilotValidation = validateInput(copilot);
@@ -86,12 +82,30 @@ function formSubmission(document, faultyItems, pilot, copilot, fuelLevel, cargoL
     if (fuelValidation === "Is a Number" && cargoValidation === "Is a Number" && fuelLevel >= 10000 && cargoLevel <= 10000) {
         document.getElementById("launchStatus").textContent = "Shuttle is Ready for Launch";
         document.getElementById("launchStatus").style.color = "green";
+        document.getElementById("faultyItems").style.visibility = "hidden"; // Hide faultyItems when conditions are met
     } else {
         // this should handle cases where conditions are not met
         document.getElementById("launchStatus").textContent = "Shuttle Not Ready for Launch";
         document.getElementById("launchStatus").style.color = "red";
         document.getElementById("faultyItems").style.visibility = "visible"; // Show faultyItems when there are issues
     }
+    */
+
+    let copilotStatus = document.getElementById("copilotStatus");
+    let pilotStatus = document.getElementById("pilotStatus");
+    let status = document.getElementById("launchStatus");
+    let cargoStatus = document.getElementById("cargoStatus");
+    let fuelStatus = document.getElementById("fuelStatus");    
+    
+    //Re-written to exclude if/else because I keep wrecking stuff. Uses ternary operators instead.
+    pilotStatus.innerHTML = `Pilot ${pilot} is ready for launch`;
+    copilotStatus.innerHTML = `Co-pilot ${copilot} is ready for launch`;
+    list.style.visibility = "visible";
+    status.style.color = fuelLevel < 10000 || cargoLevel >= 10000 ? "red" : "green";
+    status.innerHTML = fuelLevel < 10000 || cargoLevel >= 10000 ? "Shuttle Not Ready for Launch" : "Shuttle is Ready for Launch";
+    fuelStatus.innerHTML = fuelLevel < 10000 ? "Fuel level too low for launch" :  "Fuel level high enough for launch";
+    cargoStatus.innerHTML = cargoLevel >= 10000 ? "Cargo mass too heavy for launch" : "Cargo mass low enough for launch";
+
 }
  
  async function myFetch() {
